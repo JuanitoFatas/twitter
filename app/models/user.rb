3d886ea435079d869eeae09f19ee5b258f53b36f
Juanito Fatas <katehuang0320@gmail.com>
2020-03-07 14:20:01 +0900

    Add users table with lower unique fields
    email, username are in lower case and unique
    
    rails db:migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: { case_sensitive: false }
   validates :password_digest, presence: true
 end
---
+++ db/migrate/20200307051724_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :username, null: false
+      t.string :password_digest, null: false
+
+    t.index "lower((email)::text)", name: "index_users_on_lower_email", unique: true
+    t.index "lower((username)::text)", name: "index_users_on_lower_username", unique: true
+
+      t.timestamps
+    end
+  end
+end
