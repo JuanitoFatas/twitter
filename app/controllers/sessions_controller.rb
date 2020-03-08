6439776668bdb0fe1bcdcc2dcc467302c29325ca
Juanito Fatas <katehuang0320@gmail.com>
2020-03-08 16:13:50 +0900

    Sign in with email or username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create], raise: false
+  skip_before_action :require_login, only: [:new, :create, :destroy], raise: false
 
   def new
   end
 
   def create
-    user = authenticate_session(session_params)
+    user = authenticate_session(session_params, lookup_field_map)
 
     if sign_in(user)
       redirect_to root_path
     else
+      flash.now[:error] = "Invalid credentials."
       render :new
     end
   end
@@ -22,6 +23,10 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:email, :password)
+    params.require(:session).permit(:email_or_username, :password)
+  end
+
+  def lookup_field_map
+    { email_or_username: [:email, :username] }
   end
 end
