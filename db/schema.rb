d0c8bbf53767eaabc315a76162cb1446d7a578b0
Juanito Fatas <katehuang0320@gmail.com>
2020-03-09 12:51:47 +0900

    Create text_tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_022808) do
+ActiveRecord::Schema.define(version: 2020_03_09_034944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "text_tweets", force: :cascade do |t|
+    t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "body", null: false
