3031e59519d3c43c75f0258635bc67ff4e67311e
Juanito Fatas <katehuang0320@gmail.com>
2020-03-09 12:04:59 +0900

    Create tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_051724) do
+ActiveRecord::Schema.define(version: 2020_03_09_022808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_03_07_051724) do
     t.index "lower((username)::text)", name: "index_users_on_lower_username", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
