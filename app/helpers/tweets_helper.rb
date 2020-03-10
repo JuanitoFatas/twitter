d9d187706e402b617de0f82bd4e0f2adbef90038
Juanito Fatas <katehuang0320@gmail.com>
2020-03-10 10:59:03 +0900

    Pull avatar to its own

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,11 +1,4 @@
 module TweetsHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest(user.email)
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-
-    image_tag gravatar_url, class: "w-10 h-10 rounded-full mr-4"
-  end
-
   def like_button(tweet)
     if current_user.liked?(tweet)
       link_to "Unlike", unlike_tweet_path(tweet), method: :delete
