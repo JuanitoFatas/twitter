5788735f358e85ad8188c97790639e1745c41098
Juanito Fatas <katehuang0320@gmail.com>
2020-03-10 12:21:49 +0900

    see followed user tweets in timeline on dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,6 +43,10 @@ class User < ActiveRecord::Base
     followed_user_ids.include? user.id
   end
 
+  def timeline_tweets
+    Tweet.where(user_id: followed_user_ids + [id])
+  end
+
   def to_param
     username
   end
