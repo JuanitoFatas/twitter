45ca3abb8f0cc4cf880340bd95d5aeda8d1c9f56
Juanito Fatas <katehuang0320@gmail.com>
2020-03-10 11:42:44 +0900

    Style the follow unfollow button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,9 +3,9 @@ module UsersHelper
     return if current_user == user
 
     if current_user.following?(user)
-      button_to "Unfollow", follow_user_path(user), method: :delete
+      button_to "Unfollow", follow_user_path(user), method: :delete, class: "ml-2 bg-transparent hover:bg-blue-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full"
     else
-      button_to "Follow", follow_user_path(user)
+      button_to "Follow", follow_user_path(user), class: "ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
     end
   end
 end
